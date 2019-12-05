f571b97333f867bcbd176d10f6f9b90ab767a346
Javier Amado <JavierAmado7@gmail.com>
2019-12-05 10:16:15 +0100

    Ejemplo de ToolBar

---
+++ app/src/main/java/com/javieramado/toolbarpreferences/MainActivity.java
@@ -0,0 +1,43 @@
+package com.javieramado.toolbarpreferences;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Sustituye la Appbar por la Toolbar del diseño
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                return true;
+
+            case R.id.action_add:
+                Toast.makeText(this, "Se ha pulsado ", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
