edb0cf26e029489f557ef915f4d12d91e9a59ff5
Javier Amado <JavierAmado7@gmail.com>
2019-12-11 09:41:20 +0100

    se selecciona todo el texto, solo se puede poner una línea y se oculta la contraseña en el ususario y contraseña

--- app/src/main/java/com/javieramado/toolbarpreferences/AccountFragment.java
+++ app/src/main/java/com/javieramado/toolbarpreferences/AccountFragment.java
@@ -1,12 +1,46 @@
 package com.javieramado.toolbarpreferences;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class AccountFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.account_preferences);
+        initPreferenceUser();
+        initPreferencePassword();
+    }
+
+    /**
+     * Inicializa la preferencia EditTextPreference User
+     */
+    private void initPreferenceUser() {
+        EditTextPreference edpUser = getPreferenceManager().findPreference(getString(R.string.key_user));
+        edpUser.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+            @Override
+            public void onBindEditText(@NonNull EditText editText) {
+                editText.setSingleLine(true); //Que ocupe una línea
+                editText.setInputType(InputType.TYPE_CLASS_TEXT); //Que sea de tipo texto
+                editText.selectAll(); //Se seleccione al completo. Tiene que estar lo último para que funcione.
+            }
+        });
+    }
+
+    private void initPreferencePassword() {
+        EditTextPreference edpPassword = getPreferenceManager().findPreference(getString(R.string.key_password));
+        edpPassword.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+            @Override
+            public void onBindEditText(@NonNull EditText editText) {
+                editText.setSingleLine(true); //Que ocupe una línea
+                //Es necesario poner los dos tipos para que funcione
+                editText.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD); //Que sea de tipo texto
+                editText.selectAll(); //Se seleccione al completo. Tiene que estar lo último para que funcione.
+            }
+        });
     }
 }
